b640f72443031373060919b3b8ffe246f6cb19fb
yash <91825864+YashRajCodes@users.noreply.github.com>
2026-04-10 18:01:46 +0530

    feat: update discord inv link to track server joins from the site

--- src/views/Discord/index.tsx
+++ src/views/Discord/index.tsx
@@ -5,7 +5,7 @@ import { onMount } from "solid-js"
 
 export default function Discord() {
     onMount(() => {
-        window.location.href = "https://discord.gg/wKgT9j2xfN"
+        window.location.href = "https://discord.gg/EQcUE446f3"
     })
 
     return (
